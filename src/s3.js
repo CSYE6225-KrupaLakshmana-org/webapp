c2316a609e0e44ee5c147c6fe989c7fb32ec19f8
Krupa Lakshmana <lakshmana.k@northeastern.edu>
2025-10-30 08:58:14 -0400

    Fix: packer files

--- src/s3.js
+++ src/s3.js
@@ -1,17 +1,17 @@
-import { S3Client, PutObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+// src/s3.js
+import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { timePromise } from './metrics.js';
 
-export const s3 = new S3Client({ region: process.env.AWS_REGION });
+const s3 = new S3Client({ region: process.env.AWS_REGION });
 
-export async function uploadImageToS3({ bucket, key, buffer, contentType, metadata }) {
-  await s3.send(new PutObjectCommand({
-    Bucket: bucket,
-    Key: key,
-    Body: buffer,
-    ContentType: contentType,
-    Metadata: metadata || {},
-  }));
+export async function s3Put(params) {
+  return timePromise('s3.PutObject.duration_ms', s3.send(new PutObjectCommand(params)));
 }
 
-export async function deleteImageFromS3({ bucket, key }) {
-  await s3.send(new DeleteObjectCommand({ Bucket: bucket, Key: key }));
+export async function s3Get(params) {
+  return timePromise('s3.GetObject.duration_ms', s3.send(new GetObjectCommand(params)));
+}
+
+export async function s3Delete(params) {
+  return timePromise('s3.DeleteObject.duration_ms', s3.send(new DeleteObjectCommand(params)));
 }
